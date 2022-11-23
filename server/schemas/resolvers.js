2bae85c2d85c9d3d5c325f2edbfab7ca9d9b3504
BenLiu <liumanpan1990@hotmail.com>
2022-11-23 13:10:13 -0500

    initial setup

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,28 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id);
+
+        return user;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
