6d3e96b188e258da4ed688cc0962912f86250e8a
unknown <alicia9790972@gmail.com>
2022-11-23 16:57:58 -0500

    home page nav fix bug and add card

---
+++ client/src/components/Container.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Home from "../pages/Home";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
