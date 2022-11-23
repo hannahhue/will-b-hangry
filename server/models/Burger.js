a208bb1470c06e382fabf6eb945d2bde51adb1ab
ramosw1993 <ramosw1993@gmail.com>
2022-11-23 14:32:19 -0500

    models

---
+++ server/models/Burger.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const burgerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
+  },
+  topping: {
+    type: Schema.Types.ObjectId,
+    ref: 'Topping',
+    required: true
+  }
+});
+
+const Burger = model('Burger', burgerSchema);
+
+module.exports = Burger;
