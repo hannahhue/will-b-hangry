69c9445be3fa607ba2a812a796499ffe0c0098d0
unknown <alicia9790972@gmail.com>
2022-11-23 14:16:04 -0500

    navbar and home pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
 const Home = () => {
-  return <div className="container">Welcome to Will-B-Hangry</div>;
+  return (
+    <main>
+      <div class="container">
+        <div class="neon">Neon </div>
+        <div class="flux">Flux </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
