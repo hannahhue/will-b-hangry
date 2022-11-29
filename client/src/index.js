7e9ecd649a10dcfa04529bbbe6f69af532a1394f
BenLiu <liumanpan1990@hotmail.com>
2022-11-29 01:11:40 -0500

    pwa

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 //rendering app and within app.js
 ReactDOM.render(
